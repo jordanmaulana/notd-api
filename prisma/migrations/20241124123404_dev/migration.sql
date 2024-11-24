c4dc63a0b19f24c7e569e394b52be2959fd96995
jordanmaulana <jordanmaulana26@gmail.com>
2024-11-24 21:01:03 +0700

    feat: session delete cascade

---
+++ prisma/migrations/20241124123404_dev/migration.sql
@@ -0,0 +1,13 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Session" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Session" ("id", "userId") SELECT "id", "userId" FROM "Session";
+DROP TABLE "Session";
+ALTER TABLE "new_Session" RENAME TO "Session";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
